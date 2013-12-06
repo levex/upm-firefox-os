0c385a8913e9e3abc329abf374ca4081bc3934fa
Levente Kurusa <levex@linux.com>
2013-12-06 21:21:15 +0100

    Add secondary page for adding passwords
    Signed-off-by: Levente Kurusa <levex@linux.com>

---
+++ add.js
@@ -0,0 +1,5 @@
+var addBtn = document.getElementById('addBtn');
+
+addBtn.addEventListener('click', function() {
+	window.location = "index.html";
+});
---
+++ filler.js
@@ -0,0 +1,8 @@
+var pwds = document.getElementById("pwds_list");
+var addBtn = document.getElementById("thebody");
+var i = 0;
+
+/* addBtn.addEventListener('click', function() {
+	i ++;
+	pwds.innerHTML += "<li><p>" + i + "</p></li>";
+}); */
